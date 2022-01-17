9761678e85d9b0aa20fd8f2f9c946f627b73762a
jihan <jihancahya28@gmail.com>
2022-01-17 13:39:08 +0700

    filter by value udh bisa

--- app/Views/admin/print_nilai.php
+++ app/Views/admin/print_nilai.php
@@ -1,7 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<?= $this->extend('admin/template'); ?>
-<?= $this->section('content'); ?>
+<head>
 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title> Dashboard</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="/assets/startbootstrap/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="/assets/startbootstrap/https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="/assets/startbootstrap/css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- <link href="/assets/startbootstrap/css/dboard.css" rel="stylesheet"> -->
+
+    <!-- <link href="/assets/startbootstrap/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
+
+    <link rel="stylesheet" href="/assets/startbootstrap/vendor/datatables/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="/assets/startbootstrap/vendor/datatables/searchBuilder.dataTables.min.css">
+    <link rel="stylesheet" href="/assets/startbootstrap/vendor/datatables/dataTables.dateTime.min.css">
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/dashboard">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-laugh-wink"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">Evaluasi Kinerja<sup>bps</sup></div>
+        </a>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Sidebar -->
+        <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#adminUtilities" aria-expanded="true" aria-controls="adminUtilities">
+                    <i class="fas fa-fw fa-wrench"></i>
+                    <span>Admin</span>
+                </a>
+                <div id="adminUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header"></h6>
+                        <a class="collapse-item" href="/admin/print">Data Penilaian Mitra</a>
+                        <!-- <a class="collapse-item" href="/admin/data-penilaian">Data Penilaian Mitra</a> -->
+                        <a class="collapse-item" href="/admin/tambah-mitra">Tambah Data Mitra Baru</a>
+                        <a class="collapse-item" href="/admin/tambah-kegiatan">Tambah Kegiatan</a>
+                        <a class="collapse-item" href="/admin/mitra-lengkap">Data Mitra Lengkap</a>
+                       
+                    </div>
+                </div>
+             
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/manage/account">
+                    <i class="fas fa-user-cog"></i>
+                    <span>Manage Account</span></a>
+            </li>
+        <!-- Content Wrapper -->
+    </ul>
 <!-- Content Wrapper -->
 <div id="content-wrapper" class="d-flex flex-column">
 
@@ -27,7 +95,7 @@
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <table class="table table-bordered" id="example" width="100%" cellspacing="0">
                                 <thead>
                                     <tr>
                                         <th scope="col">No Urut</th>
@@ -86,4 +154,46 @@
         </div>
         <!-- End of Main Content -->
 
-        <?= $this->endSection(); ?>
+            <!-- Bootstrap core JavaScript-->
+            <script src="/assets/startbootstrap/vendor/jquery/jquery.min.js"></script>
+            <script src="/assets/startbootstrap/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+            <!-- Core plugin JavaScript-->
+            <script src="/assets/startbootstrap/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+            <!-- Custom scripts for all pages-->
+            <script src="/assets/startbootstrap/js/sb-admin-2.min.js"></script>
+
+            <!-- Page level plugins -->
+            <script src="/assets/startbootstrap/vendor/chart.js/Chart.min.js"></script>
+
+            <!-- Page level custom scripts -->
+            <script src="/assets/startbootstrap/js/demo/chart-area-demo.js"></script>
+            <script src="/assets/startbootstrap/js/demo/chart-pie-demo.js"></script>
+
+            <!-- <script src="/assets/startbootstrap/vendor/datatables/jquery.dataTables.min.js"></script>
+            <script src="/assets/startbootstrap/vendor/datatables/dataTables.bootstrap4.min.js"></script> -->
+
+            <script src="/assets/startbootstrap/vendor/datatables/jquery-3.5.1.js"></script>
+            <script src="/assets/startbootstrap/vendor/datatables/jquery.dataTables.min.js"></script>
+            <script src="/assets/startbootstrap/vendor/datatables/dataTables.searchBuilder.min.js"></script>
+            <script src="/assets/startbootstrap/vendor/datatables/dataTables.dateTime.min.js"></script>
+            <!-- Page level custom scripts -->
+           <!-- <script src="/assets/startbootstrap/js/demo/datatables-demo.js"></script> -->
+
+           <script>
+               $(document).ready(function() {
+                    $('#example').DataTable( {
+                        dom: 'Qlfrtip',
+                        searchBuilder: {
+                            columns: [9,10,11,12]
+                        }
+                    });
+                });
+           </script>
+        </div>
+    </div>
+
+</body>
+
+</html>
