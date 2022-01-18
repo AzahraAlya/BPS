6da45eba7b54705453cfa242ce946931022bed60
AzahraAlya <azahraalia9090@gmail.com>
2022-01-18 22:24:55 +0700

    change password

--- app/Models/M_user.php
+++ app/Models/M_user.php
@@ -23,31 +23,31 @@ class M_user extends Model
     protected $updatedField         = 'updated_at';
     protected $deletedField         = 'deleted_at';
 
-    // protected $beforeInsert = ['beforeInsert'];
-    // protected $beforeUpdate = ['beforeUpdate'];
+    protected $beforeInsert = ['beforeInsert'];
+    protected $beforeUpdate = ['beforeUpdate'];
   
   
   
   
-    // protected function beforeInsert(array $data){
-    //   $data = $this->passwordHash($data);
-    //   $data['data']['created_at'] = date('Y-m-d H:i:s');
+    protected function beforeInsert(array $data){
+      $data = $this->passwordHash($data);
+      $data['data']['created_at'] = date('Y-m-d H:i:s');
   
-    //   return $data;
-    // }
+      return $data;
+    }
   
-    // protected function beforeUpdate(array $data){
-    //   $data = $this->passwordHash($data);
-    //   $data['data']['updated_at'] = date('Y-m-d H:i:s');
-    //   return $data;
-    // }
+    protected function beforeUpdate(array $data){
+      $data = $this->passwordHash($data);
+      $data['data']['updated_at'] = date('Y-m-d H:i:s');
+      return $data;
+    }
   
-    // protected function passwordHash(array $data){
-    //   if(isset($data['data']['password']))
-    //     $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
+    protected function passwordHash(array $data){
+      if(isset($data['data']['password']))
+        $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
   
-    //   return $data;
-    // }
+      return $data;
+    }
   
     
 
